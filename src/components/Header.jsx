b50e1494aa5154879ddee9c3045ea02e908b1cd2
BergerMuxaujl <berger.muxaujl@gmail.com>
2021-10-02 11:48:34 +0300

    Основная верстка, дробление на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+        <div className="logoWrapper">
+            <img src="https://w7.pngwing.com/pngs/467/479/png-transparent-responsive-web-design-web-development-computer-icons-world-wide-web.png"></img>
+        </div>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
+                </div>
+                <div>Ava + description</div>
+                <div>
+                    My posts
+                    <div>New post\</div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Profile;

3b256fe719cf8b04fb4c892798da5f6c9b32d19a
BergerMuxaujl <berger.muxaujl@gmail.com>
2021-10-05 18:52:42 +0300

    Lesson 21 - Верстка вкладки Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,18 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs   
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={`${s.dialog} ${s.active}`}>Angela</div>
+                <div className={s.dialog}>Nikita</div>
+                <div className={s.dialog}>Alisa</div>
+                <div className={s.dialog}>Nikolay</div>
+                <div className={s.dialog}>Natasha</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.massage}>Privet!</div>
+                <div className={s.massage}>Che delaesh?</div>
+            </div>
         </div>
     )
 }

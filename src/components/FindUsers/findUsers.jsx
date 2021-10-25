bc7673861d380525ee1c294f7edb4d3eed99cefb
BergerMuxaujl <berger.muxaujl@gmail.com>
2021-10-25 10:51:25 +0300

    Lesson 56 - Дополнительная контейнерная компонента

--- src/components/FindUsers/findUsers.jsx
+++ src/components/FindUsers/findUsers.jsx
@@ -1,82 +1,65 @@
 import s from "./findUsers.module.css";
-import * as axios from "axios";
 import image from "../../accets/fake-ava.png";
 import React from "react";
 
-class FindUsers extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.countUsersPage}`).then((response) => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
+const FindUsers = (props) => {
+    let countPages = Math.ceil(props.totalUsersCount / props.countUsersPage);
+
+    let pages = [];
+    for (let i = 1; i <= countPages; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (num) => {
-        this.props.setCurrentPage(num);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.countUsersPage}`).then((response) => {
-            this.props.setUsers(response.data.items);
-        });
-    };
-
-    render() {
-        let countPages = Math.ceil(this.props.totalUsersCount / this.props.countUsersPage);
-
-        let pages = [];
-        for (let i = 1; i <= countPages; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div className={s.wrapperUsers}>
-                <div className={s.pagesButton}>
-                    {pages.map((num) => {
-                        return (
-                            <span
-                                onClick={(e) => {
-                                    this.onPageChanged(num);
-                                }}
-                                className={num === this.props.currentPage ? `${s.pageButtonCurrent} ${s.pageButton}` : s.pageButton}
-                            >
-                                {num}
-                            </span>
-                        );
-                    })}
-                </div>
-                {this.props.users.map((user) => (
-                    <div key={user.id} className={s.wrapperUser}>
-                        <div className={s.avaWrapper}>
-                            <div className={s.ava}>
-                                <img src={user.ava || image} alt="ava" className={s.avaImg} />
-                            </div>
-                            <div className={s.followBox}>
-                                {user.followed ? (
-                                    <button onClick={() => this.props.unfollow(user.id)} className={`${s.butFol} ${s.unfollow} `}>
-                                        Unfollow
-                                    </button>
-                                ) : (
-                                    <button onClick={() => this.props.follow(user.id)} className={`${s.butFol} ${s.follow} `}>
-                                        Follow
-                                    </button>
-                                )}
-                            </div>
+    return (
+        <div className={s.wrapperUsers}>
+            <div className={s.pagesButton}>
+                {pages.map((num) => {
+                    return (
+                        <span
+                            onClick={(e) => {
+                                props.onPageChanged(num);
+                            }}
+                            className={num === props.currentPage ? `${s.pageButtonCurrent} ${s.pageButton}` : s.pageButton}
+                        >
+                            {num}
+                        </span>
+                    );
+                })}
+            </div>
+            {props.users.map((user) => (
+                <div key={user.id} className={s.wrapperUser}>
+                    <div className={s.avaWrapper}>
+                        <div className={s.ava}>
+                            <img src={user.ava || image} alt="ava" className={s.avaImg} />
                         </div>
-                        <div className={s.userInfo}>
-                            <div className={s.userInfo_top}>
-                                <div className={s.fullName}>
-                                    <span className={s.firstName}>{user.name}</span>
-                                    <span className={s.surname}>{"user.surName"}</span>
-                                </div>
-                                <div className={s.location}>
-                                    <span className={s.city}>{"user.location.city"}</span>, <span className={s.country}>{"user.location.country"}</span>
-                                </div>
-                            </div>
-                            <div className={s.status}>{user.status}</div>
+                        <div className={s.followBox}>
+                            {user.followed ? (
+                                <button onClick={() => props.unfollow(user.id)} className={`${s.butFol} ${s.unfollow} `}>
+                                    Unfollow
+                                </button>
+                            ) : (
+                                <button onClick={() => props.follow(user.id)} className={`${s.butFol} ${s.follow} `}>
+                                    Follow
+                                </button>
+                            )}
                         </div>
                     </div>
-                ))}
-            </div>
-        );
-    }
-}
+                    <div className={s.userInfo}>
+                        <div className={s.userInfo_top}>
+                            <div className={s.fullName}>
+                                <span className={s.firstName}>{user.name}</span>
+                                <span className={s.surname}>{"user.surName"}</span>
+                            </div>
+                            <div className={s.location}>
+                                <span className={s.city}>{"user.location.city"}</span>, <span className={s.country}>{"user.location.country"}</span>
+                            </div>
+                        </div>
+                        <div className={s.status}>{user.status}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default FindUsers;
--- src/components/FindUsers/findUsersContainer.jsx
+++ src/components/FindUsers/findUsersContainer.jsx
@@ -1,6 +1,38 @@
 import { connect } from "react-redux";
 import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "./../../redux/findusers-Reducer";
 import FindUsers from "./findUsers";
+import * as axios from "axios";
+import React from "react";
+
+class FindUsersAPIComponent extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.countUsersPage}`).then((response) => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (num) => {
+        this.props.setCurrentPage(num);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.countUsersPage}`).then((response) => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    render() {
+        return (
+            <FindUsers
+                totalUsersCount={this.props.totalUsersCount}
+                countUsersPage={this.props.countUsersPage}
+                onPageChanged={this.onPageChanged}
+                currentPage={this.props.currentPage}
+                users={this.props.users}
+                unfollow={this.props.unfollow}
+                follow={this.props.follow}
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -31,6 +63,6 @@ let mapDispatchtoProps = (dispatch) => {
     };
 };
 
-const FindUsersContainer = connect(mapStateToProps, mapDispatchtoProps)(FindUsers);
+const FindUsersContainer = connect(mapStateToProps, mapDispatchtoProps)(FindUsersAPIComponent);
 
 export default FindUsersContainer;
